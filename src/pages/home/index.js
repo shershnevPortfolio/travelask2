fdf0922f9e977a5026674ed2f4df91e01ca4f04a
danil shershnev <shershnev.danil2000@gmail.com>
2019-12-02 11:13:56 +0300

    done smooth scroll to сertain block

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -5,6 +5,16 @@ export default () => {
   $('.more-info-link').on('click', function(e) {
       e.preventDefault();
       let scrollHeight = $(this).parent().parent('.header').outerHeight();
-      $('html').animate({ scrollTop: scrollHeight } ,1000);
+      $('html').animate({ scrollTop: scrollHeight } ,1500);
+      return false;
   });
-};
+
+
+  $('.view-photo-button').on('click', function(e) {
+    e.preventDefault();
+    let scrollHeight =  $('#photos-section').offset().top;
+    $('html').animate({ scrollTop: scrollHeight } ,1500);
+
+  })
+}
+
