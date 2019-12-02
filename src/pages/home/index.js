5b558987f1741ad71b516653d1fe98885c993f5f
danil shershnev <shershnev.danil2000@gmail.com>
2019-12-02 10:51:08 +0300

    done smooth scroll to next section

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,4 +1,10 @@
 import './index.sass';
 import './index.pug';
 
-export default () => {};
+export default () => {
+  $('.more-info-link').on('click', function(e) {
+      e.preventDefault();
+      let scrollHeight = $(this).parent().parent('.header').outerHeight();
+      $('html').animate({ scrollTop: scrollHeight } ,1000);
+  });
+};
