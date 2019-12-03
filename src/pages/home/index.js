e504265f344e2462a74a65dad7f7e7e336f0aa68
danil shershnev <shershnev.danil2000@gmail.com>
2019-12-03 11:47:56 +0300

    deleted some trash

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -54,8 +54,6 @@ carousel.owlCarousel({
     let currentSlideNumber = event.item.index + 1;
     let outputString = currentSlideNumber >= 10?  currentSlideNumber : `0${currentSlideNumber}`;
     $('.slider-toggle__number--left').text(outputString);
-    console.log(`current: ${+currentSlideNumber}`);
-    console.log(`at all ${+event.item.count}`);
     if(+currentSlideNumber  == +event.item.count) {
       $('.slider-toggle__number--right').addClass('slider-toggle__number--disabled');
       $('.slider-toggle__number--left').removeClass('slider-toggle__number--disabled');
@@ -68,39 +66,15 @@ carousel.owlCarousel({
 
 
 
-  // console.log(carousel.find('.photos__container').length);
   $('.slider-toggle__link--right').on('click', function(e) {
     e.preventDefault();
-  //   let currentSlideNumber = $('.slider-toggle__number--left').text();
-  //   currentSlideNumber++;
-  //   if(currentSlideNumber > numberOfSliderItems) {return false;};
     carousel.trigger('next.owl.carousel');
-    // console.log(e.item.index + 'sad');
-    // if(currentSlideNumber < 10) {
-
-    //   return false;
-    // }
-
-  //   $('.slider-toggle__number--disabled').text(currentSlideNumber);
-  //   return false;
 
   })
 
   $('.slider-toggle__link--left').on('click', function (e) {
     e.preventDefault();
-    // console.log($('.slider-toggle__number--left').text());
-    // let currentSlideNumber = $('.slider-toggle__number--left').text();
-    // console.log(currentSlideNumber);
-    // currentSlideNumber--;
-    // if(currentSlideNumber < 1) {return false};
     carousel.trigger('prev.owl.carousel');
-    // if(currentSlideNumber < 10) {
-    //   $('.slider-toggle__number--left').text(`0${currentSlideNumber}`);
-    //   return false;
-    // }
-    // $('.slider-toggle__number--left').text(currentSlideNumber);
-
-
   })
 
 
